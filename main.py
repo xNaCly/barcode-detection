5d2ab53988bf6115c3bd5205f2600bade86a09f3
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-11-13 16:12:25 +0100

    added comments, etc.

--- main.py
+++ main.py
@@ -22,16 +22,39 @@ class BarCodeDector:
     """
     border_color: tuple[int, int, int] 
     """
-    Color to apply to the border drawn around the detected barcode, format: rgb
+    Color to apply to the border drawn around the detected barcode, format: rgb, defaults to (0,255,0)
     """
 
     def __init__(self, image_path: str, border_color: str):
+        """
+        Detects bar codes in the given image, draws a border around them. 
+
+        Algorithm outline:
+            1. sharr gradient in x and y direction
+            2. subtract y from x gradient for finding barcode region
+            3. blur and threshold
+            4. Closing kernel
+            5. Series of dilations and erosions
+            6. Largest contour should be the barcode
+
+        @args image_path path to the input image
+        @args border_color rgb color in format r:g:b
+        @error ValueError if image_path is empty
+        """
+        # image path is required
         if not image_path:
             raise ValueError("Expected 'image_path' to be a value, not None")
         self.image_path = image_path
         self.border_color = self.string_rgb_to_color(border_color)
 
     def string_rgb_to_color(self, rgb_fmt: str) -> tuple[int, int, int]:
+        """
+        converts rgb_fmt of format 'r:g:b' to a tuple [r, g, b] all of type
+        int. 
+
+        @arg rgb_fmt input format
+        @error ValueError if invalid amount of arguments in format or number couldn't be parsed
+        """
         c = rgb_fmt.split(":")
 
         if len(c) != 3:
@@ -46,29 +69,83 @@ class BarCodeDector:
         return (c[0], c[1], c[2])
 
     def show_image(self, image):
+        """
+        Displays the image via matplotlib
+
+        @arg image image to display
+        """
         plt.imshow(image)
         plt.show()
 
     def sharr_gradient(self, image: Image) -> Image:
+        """
+        Applies the sharr gradient[1] to the given image, applies
+        convertScaleAbs[2] to the result and returns it, this highlights the
+        barcode region.
+
+        [1]: https://en.wikipedia.org/wiki/Sobel_operator
+        [2]: https://docs.opencv.org/3.4/d2/de8/group__core__array.html#ga3460e9c9f37b563ab9dd550c4d8c4e7d
+
+        @arg image image to process
+        """
         x_gradient = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
         y_gradient = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
         result = cv2.subtract(x_gradient, y_gradient)
+
+        # converts to 8bit img while scaling and computing absolut values
         return cv2.convertScaleAbs(result)
 
     def filter_noise(self, image: Image) -> Image:
-        blur = cv2.blur(image, (9,9), 0)
-        (_, thresh) = cv2.threshold(blur, 225, 255, cv2.THRESH_BINARY)
+        """
+        Attempts to filter possibly obstructing noise out of the given image by
+        blurring with the gaussian method[1] and applying a treshold[2].
+
+        [1]: https://docs.opencv.org/3.4/d4/d86/group__imgproc__filter.html#gaabe8c836e97159a9193fb0b11ac52cf1
+        [2]: https://docs.opencv.org/3.4/d7/d1b/group__imgproc__misc.html#gae8a4a146d1ca78c626a53577199e9c57
+
+        @arg image image to process
+        """
+        # (9,9) worked the best for filtering hard edges
+        blur = cv2.GaussianBlur(image, (9,9), 0)
+
+        # treshold found by testing to be 237
+        (_, thresh) = cv2.threshold(blur, 237, 255, cv2.THRESH_BINARY)
         return thresh
 
     def closing_kernel(self, image: Image) -> Image:
-        k = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+        """
+        Applies a closing operation[1] with a cv2.MORPH_RECT kernel[2] to the image
+
+        [1]: https://docs.opencv.org/4.x/d4/d86/group__imgproc__filter.html#ga7be549266bad7b2e6a04db49827f9f32
+        [2]: https://docs.opencv.org/4.x/d4/d86/group__imgproc__filter.html#gac2db39b56866583a95a5680313c314ad
+
+        @arg image image to process
+        """
+        k = cv2.getStructuringElement(cv2.MORPH_RECT, (29, 29))
         return cv2.morphologyEx(image, cv2.MORPH_CLOSE, k)
 
     def eroding_dilation(self, image: Image) -> Image:
+        """
+        Used for removing boundaries of foreground objects. Eroding removes
+        white noise[1] while shrinking the object, therefore we dilate[2] the object.
+
+        [1]: https://docs.opencv.org/4.x/d4/d86/group__imgproc__filter.html#gaeb1e0c1033e3f6b891a25d0511362aeb
+        [2]: https://docs.opencv.org/4.x/d4/d86/group__imgproc__filter.html#ga4ff0f3318642c4f469d0e11f242f3b6c
+
+        @arg image image to process
+        """
         image = cv2.erode(image, None, iterations = 4)
         return cv2.dilate(image, None, iterations = 4)
 
     def contours(self, image: Image) -> np.intp:
+        """
+        Computes the contours / borders of the detected barcode and returns
+        their edge points as np.intp
+
+        @arg image image to process
+        @returns contours
+        """
+        # returns 
         (cnts, _) = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         c = max(cnts, key=cv2.contourArea)
 
@@ -77,17 +154,25 @@ class BarCodeDector:
         return np.intp(box)
 
     def draw_box(self, image: Image, box: np.intp) -> Image:
+        """
+        Draws the contours onto the given image with the contours using the
+        color set with self.border_color
+
+        @arg image image to process
+        @arg box contour points
+        """
         return cv2.drawContours(image, [box], -1, self.border_color, 3)
 
     def process(self):
         """
-        Main function for processing the image
+        Apply barcode detection algorithm to the image located at self.image_path
         """
         og_img = cv2.imread(self.image_path)
         # original image
         # self.show_image(image)
 
-        # image is already greyscale, no need to convert (or so i thought got a cv2 error format mismatch, ig i have to greyscale this)
+        # image is already greyscale, no need to convert (or so i thought. I
+        # got a cv2 error format mismatch, ig i have to greyscale this)
         image = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
 
         image = self.sharr_gradient(image)
@@ -111,6 +196,8 @@ class BarCodeDector:
 
         self.show_image(image)
 
+# used for checking if the script is included via an other script or it is ran
+# directly
 if __name__ == "__main__":
     flag = argparse.ArgumentParser()
     flag.add_argument("-p", "--path", help="Path to the input image", required=True)
